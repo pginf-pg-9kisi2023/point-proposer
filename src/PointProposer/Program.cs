ba05a607642f4e28eac062c1e0cdae9e9a0acd24
Mikołaj Morozowski <themix240@gmail.com>
2023-06-13 21:29:02 +0000

    Style fixes

--- src/PointProposer/Program.cs
+++ src/PointProposer/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+
 
 
 namespace PointProposer
@@ -9,15 +9,15 @@ namespace PointProposer
         static void Main(string[] args)
         {
             Random random = new Random();
-            int imageNumber = Int32.Parse(args[0]);
+            int imageNumber = Int32.Parse(args[0], System.Globalization.CultureInfo.CurrentCulture);
             int i = 1;
             while(i < args.Length) {
                 string imgName = args[i++];
                 Console.WriteLine(imgName);
-                int labelCount = Int32.Parse(args[i++]);
+                int labelCount = Int32.Parse(args[i++], System.Globalization.CultureInfo.CurrentCulture);
                 for(int k=0; k<labelCount; k++)
                 {
-                    Console.WriteLine("{0} {1} {2}", args[i++],random.NextDouble(),random.NextDouble());
+                    Console.WriteLine("{0} {1} {2}", args[i++], random.NextDouble() ,random.NextDouble());
                 }
             }
 
