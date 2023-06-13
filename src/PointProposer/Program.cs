8bdb4d55cdefd3578f7e26788dc291bd1f0f1d62
Mikołaj Morozowski <themix240@gmail.com>
2023-06-13 17:23:06 +0000

    Initial simple setup.

--- src/PointProposer/Program.cs
+++ src/PointProposer/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 
 namespace PointProposer
 {
@@ -6,7 +8,20 @@ namespace PointProposer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            int imageNumber = Int32.Parse(args[0]);
+            int i = 1;
+            while(i < imageNumber) {
+                string imgName = args[i++];
+                Console.WriteLine(imgName);
+                int labelCount = Int32.Parse(args[i++]);
+                for(int k=0; k<labelCount; k++)
+                {
+                    Console.WriteLine("{0} {1} {2}", args[i++],random.NextDouble(),random.NextDouble());
+                }
+            }
+
+
         }
     }
 }
