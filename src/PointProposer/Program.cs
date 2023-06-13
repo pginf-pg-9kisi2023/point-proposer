f99447f260e7c41aca44deab9318d31eb9b9baad
Mikołaj Morozowski <themix240@gmail.com>
2023-06-13 21:10:21 +0000

    Simple application template.

--- src/PointProposer/Program.cs
+++ src/PointProposer/Program.cs
@@ -11,7 +11,7 @@ namespace PointProposer
             Random random = new Random();
             int imageNumber = Int32.Parse(args[0]);
             int i = 1;
-            while(i < imageNumber) {
+            while(i < args.Length) {
                 string imgName = args[i++];
                 Console.WriteLine(imgName);
                 int labelCount = Int32.Parse(args[i++]);
