3629cb3a0c7b2a1fe5619ccd7843bc71cb89d545
Jakub Kuczys <me@jacken.men>
2023-06-14 04:41:00 +0200

    Randomize labels based on image size
    This adds a NuGet dependency on System.Drawing.Common
    which is fine since this is just a sample project.

--- src/PointProposer/Program.cs
+++ src/PointProposer/Program.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Drawing;
 
 
 namespace PointProposer
@@ -8,17 +8,28 @@ namespace PointProposer
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int imageNumber = Int32.Parse(args[0], System.Globalization.CultureInfo.CurrentCulture);
-            int i = 1;
+            var random = new Random();
+            var imageCount = Int32.Parse(
+                args[0], System.Globalization.CultureInfo.CurrentCulture
+            );
+            var i = 1;
             while (i < args.Length)
             {
-                string imgName = args[i++];
-                Console.WriteLine(imgName);
-                int labelCount = Int32.Parse(args[i++], System.Globalization.CultureInfo.CurrentCulture);
-                for (int k = 0; k < labelCount; k++)
+                string imgPath = args[i++];
+                using var img = new Bitmap(imgPath);
+                Console.WriteLine(imgPath);
+
+                var labelCount = Int32.Parse(
+                    args[i++], System.Globalization.CultureInfo.CurrentCulture
+                );
+                for (var k = 0; k < labelCount; k++)
                 {
-                    Console.WriteLine("{0} {1} {2}", args[i++], random.NextDouble(), random.NextDouble());
+                    Console.WriteLine(
+                        "{0} {1} {2}",
+                        args[i++],
+                        random.Next(0, img.Width),
+                        random.Next(0, img.Height)
+                    );
                 }
             }
         }
