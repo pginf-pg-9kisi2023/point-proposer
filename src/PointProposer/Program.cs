caf8832ae337b6995c5c71270444ce668d9f7351
Jakub Kuczys <me@jacken.men>
2023-06-14 03:37:52 +0200

    Fix style

--- src/PointProposer/Program.cs
+++ src/PointProposer/Program.cs
@@ -11,17 +11,16 @@ namespace PointProposer
             Random random = new Random();
             int imageNumber = Int32.Parse(args[0], System.Globalization.CultureInfo.CurrentCulture);
             int i = 1;
-            while(i < args.Length) {
+            while (i < args.Length)
+            {
                 string imgName = args[i++];
                 Console.WriteLine(imgName);
                 int labelCount = Int32.Parse(args[i++], System.Globalization.CultureInfo.CurrentCulture);
-                for(int k=0; k<labelCount; k++)
+                for (int k = 0; k < labelCount; k++)
                 {
-                    Console.WriteLine("{0} {1} {2}", args[i++], random.NextDouble() ,random.NextDouble());
+                    Console.WriteLine("{0} {1} {2}", args[i++], random.NextDouble(), random.NextDouble());
                 }
             }
-
-
         }
     }
 }
