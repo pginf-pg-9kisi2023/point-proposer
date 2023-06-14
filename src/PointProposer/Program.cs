153df90c7bd59647f566b059ec50578543da7d5c
Mikołaj Morozowski <themix240@gmail.com>
2023-06-14 04:45:48 +0200

    Create a basic app returning random points for given images/labels (#1)
    Co-authored-by: Jakub Kuczys <me@jacken.men>

--- src/PointProposer/Program.cs
+++ src/PointProposer/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Drawing;
+
 
 namespace PointProposer
 {
@@ -6,7 +8,30 @@ namespace PointProposer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var imageCount = Int32.Parse(
+                args[0], System.Globalization.CultureInfo.CurrentCulture
+            );
+            var i = 1;
+            while (i < args.Length)
+            {
+                string imgPath = args[i++];
+                using var img = new Bitmap(imgPath);
+                Console.WriteLine(imgPath);
+
+                var labelCount = Int32.Parse(
+                    args[i++], System.Globalization.CultureInfo.CurrentCulture
+                );
+                for (var k = 0; k < labelCount; k++)
+                {
+                    Console.WriteLine(
+                        "{0} {1} {2}",
+                        args[i++],
+                        random.Next(0, img.Width),
+                        random.Next(0, img.Height)
+                    );
+                }
+            }
         }
     }
 }
